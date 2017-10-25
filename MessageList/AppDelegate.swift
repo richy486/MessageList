1d900ffe37ea9e89b17dde357dc861d26e1c2b34
Richard Adem <richy486@gmail.com>
2017-10-25 09:31:44 -0400

    First commit

---
+++ MessageList/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  MessageList
+//
+//  Created by Richard Adem on 25/10/17.
+//  Copyright © 2017 Richard Adem. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        if let window = window {
+            let viewController = ViewController()
+            window.rootViewController = viewController
+            window.backgroundColor = UIColor.white
+            window.makeKeyAndVisible()
+        }
+        
+        return true
+    }
+}
---
+++ Pods/Target Support Files/Pods-MessageList/Pods-MessageList-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MessageList : NSObject
+@end
+@implementation PodsDummy_Pods_MessageList
+@end
